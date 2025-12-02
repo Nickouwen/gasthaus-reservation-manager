e4356ada96977cc559bab83a481266be1890cd64
Nicolaas <71842732+Nickouwen@users.noreply.github.com>
2025-12-01 19:23:25 -0800

    Added update method for reservations

--- reservation-service/src/main/java/com/gasthaus/reservation_service/controller/ReservationController.java
+++ reservation-service/src/main/java/com/gasthaus/reservation_service/controller/ReservationController.java
@@ -1,10 +1,14 @@
 package com.gasthaus.reservation_service.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +18,7 @@ import com.gasthaus.reservation_service.dto.ReservationResponseDTO;
 import com.gasthaus.reservation_service.service.ReservationService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 
 @RestController
 @RequestMapping("/reservations")
@@ -33,6 +38,15 @@ public class ReservationController {
     @PostMapping
     public ResponseEntity<ReservationResponseDTO> createReservation(@Valid @RequestBody ReservationRequestDTO reservationRequestDTO) {
         ReservationResponseDTO reservationResponseDTO = reservationService.createReservation(reservationRequestDTO);
+        return ResponseEntity.ok().body(reservationResponseDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ReservationResponseDTO> updateReservation(
+        @PathVariable UUID id,
+        @Validated({Default.class}) @RequestBody ReservationRequestDTO reservationRequestDTO) {
+        ReservationResponseDTO reservationResponseDTO = reservationService.updateReservation(id, reservationRequestDTO);
+
         return ResponseEntity.ok().body(reservationResponseDTO);
     }
 }
