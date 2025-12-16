3a9c7153fe09fb7ef1d218b1676ad0db3a478b0b
Nicolaas <71842732+Nickouwen@users.noreply.github.com>
2025-12-15 22:54:01 -0800

    Replaced pom.xml
    Copied tutorial's from GitHub due to me skipping the protobuf section, then having to implement it anyways (got turned around)

--- reservation-service/src/main/java/com/gasthaus/reservation_service/kafka/KafkaProducer.java
+++ reservation-service/src/main/java/com/gasthaus/reservation_service/kafka/KafkaProducer.java
@@ -3,6 +3,10 @@ package com.gasthaus.reservation_service.kafka;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import com.gasthaus.reservation_service.models.Reservation;
+
+import reservation.events.ReservationEvent;
+
 @Service
 public class KafkaProducer {
 
@@ -10,5 +14,18 @@ public class KafkaProducer {
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Reservation reservation) {
+        ReservationEvent event = ReservationEvent.newBuilder()
+                .setReservationId(reservation.getId().toString())
+                .setName(reservation.getName())
+                .setEmail(reservation.getEmail())
+                .setNumberOfGuests(reservation.getNumberOfGuests())
+                .setTimestamp(reservation.getReservationDateTime().toString())
+                .setEventType("RESERVATION_CREATED")
+                .build();
+
+        kafkaTemplate.send("reservation-events", event.toByteArray());
     }
 }
